f5a35044b0627c403475c5b9c912d668533368be
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-08-22 06:14:44 +0200

    Update nl_NL, thanks to watertoren5044

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -22,12 +22,12 @@
 // +-----------------------------------------------------------------------+
 $lang['Configuration'] = 'Configuratie';
 $lang['Url'] = 'Url';
-$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '("Automatische aanvullen" wordt automatisch geschoond bij uploads, wijzigingen in toestemmingen ...)';
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '("Automatisch aanvullen" wordt automatisch geschoond bij uploads, wijzigingen in toestemmingen ...)';
 $lang['Create a new suggestion'] = 'Maak een nieuwe suggestie';
 $lang['Invalidate auto completion index cache'] = 'De index cache van het "automatisch aanvullen" te legen';
 $lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'Leeg laten voor een automatische galerie zoekopdracht <br>OF geef een url ($r/ aan het begin van de url betekent deze galerie)<br>OF geef q=Term om te zoeken naar een specifieke term';
-$lang['If you change album/tag names or permalink you might want to'] = 'Indien u album-/label-namen of permalinks heeft gewijzigd is het wellicht aan te raden om';
-$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Gebruik <b>\</b> om het zoeken te onderbreken (bv Central Park\, NY komt niet overeen met NY maar zal het wel weergeven)';
+$lang['If you change album/tag names or permalink you might want to'] = 'Als je album-/label-namen of permalinks hebt gewijzigd is het wellicht aan te raden om';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Gebruik <b>\</b> om het zoeken te onderbreken (bv Central Park\, NY komt niet overeen met NY maar toont het wel)';
 $lang['Custom'] = 'Aangepast';
 $lang['Exclude'] = 'Uitsluiten';
 $lang['Query'] = 'Vraag';
@@ -35,6 +35,6 @@ $lang['Weight'] = 'Gewicht';
 $lang['Variants'] = 'Varianten';
 $lang['Create new variants'] = 'Maak nieuwe varianten';
 $lang['Ignore it and'] = 'Negeer het en';
-$lang['Search also for any of the following'] = 'Zoek ook naar een van de volgende';
+$lang['Search also for any of the following'] = 'Zoek ook naar één van de volgende';
 $lang['Search it and'] = 'Zoek het en';
-$lang['When any of the following words is entered'] = 'Als een van de volgende woorden wordt ingevoerd';
+$lang['When any of the following words is entered'] = 'Als één van de volgende woorden wordt ingevoerd';
