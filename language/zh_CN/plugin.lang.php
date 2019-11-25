2d82bd9bfb63555eae55315ae9cf23190dd364c5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-25 06:06:41 +0100

    Update zh_CN, thanks to Arnie97

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -20,21 +20,21 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Url'] = '链接';
-$lang['Weight'] = '重量（Weight）';
+$lang['Url'] = 'URL';
+$lang['Weight'] = '权重';
 $lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '（自动完成功能在上传、权限改变时将自动失效…）';
 $lang['Configuration'] = '配置';
 $lang['Create a new suggestion'] = '创建新的建议';
 $lang['Custom'] = '自定义';
 $lang['Exclude'] = '排除';
-$lang['If you change album/tag names or permalink you might want to'] = '如果你改变了相册/标签名称或者固定链接你可能会想要';
-$lang['Invalidate auto completion index cache'] = '禁用搜索建议缓存';
-$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = '留空使相册自动搜索<br>或者输入一个链接（以$r/开头表示此相册）<br>或者输入 q=检索词 以搜索特定的检索词';
-$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = '使用<b>\</b>分隔搜索词（如：Central Park\, NY 将不会匹配 NY，但会增加显示 NY）';
+$lang['If you change album/tag names or permalink you might want to'] = '如果您改变了相册/标签的名称或者固定链接，您可能会想要';
+$lang['Invalidate auto completion index cache'] = '重置搜索建议缓存';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = '留空来自动搜索相关相册<br>或者输入一个链接（URL 开头可以用 $r/ 表示此相册）<br>或者输入 q=检索词 以搜索特定的检索词';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = '使用<b>\</b>来中断搜索（如：Central Park\, NY 将不会搜索 NY，但会在下拉框中显示 NY）';
 $lang['Query'] = '查询';
-$lang['Search also for any of the following'] = '也搜索以下内容';
-$lang['Search it and'] = '搜索并';
+$lang['Search also for any of the following'] = '同时搜索以下内容';
+$lang['Search it and'] = '搜索输入的内容，并';
 $lang['Variants'] = '变体';
 $lang['When any of the following words is entered'] = '当输入下列任一词时';
 $lang['Create new variants'] = '创建新的变体';
-$lang['Ignore it and'] = '忽略并';
+$lang['Ignore it and'] = '忽略输入的内容，并';
