0e5210032909102baeebb66a2561d3854f66ba6c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-03-26 16:18:48 +0100

    Add ja_JP, thanks to Toramaru13

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,20 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = 'オートコンプリートはアップロードや権限の変更などの際には自動的にオフになります';
+$lang['Configuration'] = '設定';
+$lang['Create a new suggestion'] = '新しい提案を作成';
+$lang['Create new variants'] = '新しいバリエーションを作成';
+$lang['Custom'] = 'カスタム';
+$lang['Exclude'] = '除外';
+$lang['Ignore it and'] = 'これを無視し、そして';
+$lang['Invalidate auto completion index cache'] = 'オートコンプリートインデックスキャッシュを無効にする';
+$lang['Query'] = 'クエリ';
+$lang['Search it and'] = 'これを検索し、そして';
+$lang['Url'] = 'URL';
+$lang['Variants'] = 'バリエーション';
+$lang['Weight'] = '重さ';
