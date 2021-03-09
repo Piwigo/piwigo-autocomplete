007136f94db8b8ca6987204950f28e7405dcfa27
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-03-09 15:36:08 +0100

    Update ca_ES, thanks to Sebastia

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '(Les complecions automàtiques s\'invaliden automàticament en les pujades, els canvis de permisos...)';
+$lang['Configuration'] = 'Configuració';
+$lang['Create a new suggestion'] = 'Creeu un suggeriment nou';
+$lang['Create new variants'] = 'Creeu variants noves';
+$lang['Custom'] = 'Personalitzat';
+$lang['Exclude'] = 'Exclou';
+$lang['If you change album/tag names or permalink you might want to'] = 'Si canvieu els noms d\'àlbum/etiquetes o l\'enllaç permanent, és possible que vulgueu';
+$lang['Ignore it and'] = 'Ignora-ho i';
+$lang['Invalidate auto completion index cache'] = 'Invalida la memòria cau de l\'índex de compleció automàtica';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'Deixeu-ho en blanc per a la cerca automàtica de galeria <br>o introduïu un URL ($r/ a l\'inici de l\'URL significa aquesta galeria)<br>o introduïu q=Terme per cercar un terme específic';
+$lang['Query'] = 'Consulta';
+$lang['Search also for any of the following'] = 'Cerca també qualsevol dels següents';
+$lang['Search it and'] = 'Cerca-ho i';
+$lang['Url'] = 'Url';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Utilitzeu <b>\</b> per interrompre la cerca (p. ex. Antoni Gaudí\, CAT no coincidirà amb CAT però la mostrarà)';
+$lang['Variants'] = 'Variants';
+$lang['Weight'] = 'Pes';
+$lang['When any of the following words is entered'] = 'Quan s\'introdueix qualsevol de les paraules següents';
