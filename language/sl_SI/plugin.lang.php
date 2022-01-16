5bc0a4151dd867f5275745675ab2c88010135b47
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-01-16 19:25:04 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Search it and'] = 'Išči in';
+$lang['Variants'] = 'Različice';
+$lang['Weight'] = 'Teža';
+$lang['When any of the following words is entered'] = 'Ko je vnesena katera koli od naslednjih besed';
+$lang['Ignore it and'] = 'Ignorirajte in';
+$lang['Query'] = 'Poizvedba';
+$lang['Search also for any of the following'] = 'Poiščite tudi karkoli od naslednjega';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Uporabite <b>\</b> za prekinitev iskanja (npr. Central Park\, NY se ne bo ujemal z NY, vendar ga bo prikazal)';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'Pustite prazno za samodejno iskanje po galeriji <br>ALI vnesite url ($r/ na začetku url pomeni to galerijo)<br>ALI vnesite q=Izraz za iskanje določenega izraza';
+$lang['Invalidate auto completion index cache'] = 'Razveljavi predpomnilnik indeksa samodejnega dokončanja';
+$lang['Create new variants'] = 'Ustvarite nove različice';
+$lang['Custom'] = 'Po meri';
+$lang['Exclude'] = 'Izločiti';
+$lang['If you change album/tag names or permalink you might want to'] = 'Če spremenite imena albumov/oznak ali stalne povezave, boste morda želeli';
+$lang['Url'] = 'Url';
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '(Samodejni zaključki so samodejno razveljavljeni pri nalaganjih, spremembah dovoljenj ...).';
+$lang['Configuration'] = 'Konfiguracija';
+$lang['Create a new suggestion'] = 'Ustvarite nov predlog';
