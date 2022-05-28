bfa1d0693712e838fc4ccd0f003c72bcfea35301
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-05-28 09:27:50 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = 'Sjálfvirkar útfyllingar eru sjálfkrafa gerðar ógildar við upphleðslur, breytingar á heimildum ...).';
+$lang['Configuration'] = 'Stillingar';
+$lang['Create a new suggestion'] = 'Búðu til nýja tillögu';
+$lang['Create new variants'] = 'Búðu til ný afbrigði';
+$lang['Custom'] = 'Sérsniðin';
+$lang['Exclude'] = 'Útiloka';
+$lang['If you change album/tag names or permalink you might want to'] = 'Ef þú breytir nafni albúms/merkja eða fastkrækju gætirðu viljað það';
+$lang['Ignore it and'] = 'Hunsa það og';
+$lang['Invalidate auto completion index cache'] = 'Ógilda skyndiminni sjálfvirkrar útfyllingar';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'Skildu eftir autt fyrir sjálfvirka galleríleit <br>EÐA sláðu inn slóð ($r/ í upphafi slóðar þýðir þetta gallerí)<br>EÐA sláðu inn q=Term til að leita að tilteknu hugtaki';
+$lang['Query'] = 'Fyrirspurn';
+$lang['Search also for any of the following'] = 'Leitaðu einnig að einhverju af eftirfarandi';
+$lang['Search it and'] = 'Leitaðu að því og';
+$lang['Url'] = 'Vefslóð';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'Notaðu <b>\</b> til að brjóta leit (t.d. Central Park\, NY mun ekki passa við NY en það mun birta það)';
+$lang['Variants'] = 'Afbrigði';
+$lang['Weight'] = 'Þyngd';
+$lang['When any of the following words is entered'] = 'Þegar eitthvað af eftirfarandi orðum er slegið inn';
