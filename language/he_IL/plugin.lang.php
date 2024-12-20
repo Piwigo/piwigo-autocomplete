e4d834078923f38a7a114e0cf4d12cc60ea2fe6f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:03:33 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,25 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['(Auto completions are automatically invalidated on uploads, permission changes ...).'] = '(השלמות אוטומטיות מבוטלות אוטומטית בעת העלאת קבצים, שינויי הרשאות ...).';
+$lang['Configuration'] = 'הגדרות';
+$lang['Create a new suggestion'] = 'צור הצעה חדשה';
+$lang['Create new variants'] = 'צור וריאנטים חדשים';
+$lang['Custom'] = 'מותאם אישית';
+$lang['Exclude'] = 'אי הכללה';
+$lang['If you change album/tag names or permalink you might want to'] = 'אם תשנה את שמות האלבומים/תגיות או את קישור הקבוע ייתכן שתרצה ל';
+$lang['Ignore it and'] = 'להתעלם מזה ו';
+$lang['Invalidate auto completion index cache'] = 'לבטל את מטמון האינדקס של השלמה אוטומטית';
+$lang['Leave empty for automatic gallery search <br>OR enter an url ($r/ at the start of url means this gallery)<br>OR enter q=Term to search for a specific term'] = 'השאר ריק לחיפוש גלריה אוטומטי<br>או הכנס כתובת URL ($r/ בתחילת ה-URL אומר שזו הגלריה הזו)<br>או הכנס q=מונח כדי לחפש מונח מסוים';
+$lang['Query'] = 'שאילתה';
+$lang['Search also for any of the following'] = 'חפש גם אחר';
+$lang['Search it and'] = 'חפש את זה ו';
+$lang['Url'] = 'כתובת URL';
+$lang['Use <b>\</b> to break search (e.g. Central Park\, NY will not match NY but it will display it)'] = 'השתמש<b>\</b> כדי לשבור את החיפוש (לדוגמה, פארק המרכזי\, ניו יורק לא יתאים ל-NY אבל יציג אותו)';
+$lang['Variants'] = 'וריאנטים';
+$lang['Weight'] = 'משקל';
+$lang['When any of the following words is entered'] = 'כאשר נכנסים אחד מהמילים הבאות';
